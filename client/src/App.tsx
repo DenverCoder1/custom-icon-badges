9d5d045ee55738af04fcf4b315b3f41276f55f53
Jonah Lawrence <jonah@freshidea.com>
2021-07-18 17:48:26 +0300

    Updated UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Coming soon!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a className="App-link" href="https://github.com/DenverCoder1/custom-icon-badges">GitHub</a>
       </header>
     </div>
   );

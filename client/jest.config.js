572cc36ca5f1ae04a0b915e8b1a29c00a89d3d78
Jonah Lawrence <jonah@freshidea.com>
2021-08-05 20:20:34 +0300

    transform bootstrap

---
+++ client/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    // transform files with ts-jest
+    "^.+\\.(js|ts|jsx|tsx)$": "ts-jest",
+  },
+  transformIgnorePatterns: [
+    // allow transformation
+    "node_modules/(?!bootstrap)"
+  ],
+  globals: {
+    "ts-jest": {
+      tsconfig: {
+        // allow js in typescript
+        allowJs: true,
+      },
+    },
+  },
+};

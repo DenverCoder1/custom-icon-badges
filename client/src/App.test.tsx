31e3a57b037ce7e7a5dc1ae891844f1af787747d
Jonah Lawrence <jonah@freshidea.com>
2022-05-18 19:09:18 -0600

    chore: Fix heroku build process (#419)

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders github link', () => {
   render(<App />);
   const linkElement = screen.getByText(/github/i);
   expect(linkElement).toBeInTheDocument();

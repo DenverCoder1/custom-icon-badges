a90415d514206c7be8727e8c0519c3928ef6300a
Jonah Lawrence <jonah@freshidea.com>
2022-05-18 19:05:35 -0600

    fix testing library react

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-// @ts-ignore
-import { expect, render, screen, test } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders github link', () => {
   render(<App />);
   const linkElement = screen.getByText(/github/i);
   expect(linkElement).toBeInTheDocument();

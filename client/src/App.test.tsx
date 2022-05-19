b6cafdc0473e151f810ba83e5a02a93e040e41d3
Jonah Lawrence <jonah@freshidea.com>
2022-05-18 18:40:02 -0600

    ts ignore

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';  // @ts-ignore  
+// @ts-ignore
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {

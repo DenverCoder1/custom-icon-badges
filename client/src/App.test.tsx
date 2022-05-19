a1db3f77b165d3dbc07e80ff81003e6813fdcb9e
Jonah Lawrence <jonah@freshidea.com>
2022-05-18 18:32:04 -0600

    import expect

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // @ts-ignore
-import { test, render, screen } from '@testing-library/react';
+import { test, render, screen, expect } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {

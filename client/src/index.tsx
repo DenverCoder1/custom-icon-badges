4ab8a4d13e029d87fd53ec5a120f29b39393777b
Jonah Lawrence <jonah@freshidea.com>
2023-08-10 13:39:59 +0300

    refactor: Replace ReactDOM.render with createRoot

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.scss";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );

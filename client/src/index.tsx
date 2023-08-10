87a756d922b1579146865e848d5a24719e4b3cc9
Jonah Lawrence <jonah@freshidea.com>
2023-08-10 13:40:31 +0300

    Update index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./index.scss";
-import App from "./App";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.scss';
+import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
